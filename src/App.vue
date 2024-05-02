ecc14f1c95a862208901544d8c720e0f2c16b92e
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-05-02 12:38:14 +0300

    tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,37 @@
 </script>
 
 <template>
-jo
+
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <header class="flex justify-between ">
+
+      <div>
+        <img src="/logo.png" alt="logo" />
+        <div>
+          <h2>Vue Sneakers</h2>
+          <p>Магазин лучших кросовок</p>
+        </div>
+      </div>
+
+      <ul>
+        <li>
+          <img src="/cart.svg" alt="Cart" />
+          <b>1205 руб.</b>
+        </li>
+
+        <li>
+          <img src="/heart.svg" alt="Cart" />
+          <b>Закладки</b>
+        </li>
+
+        <li>
+          <img src="/profile.svg" alt="Cart" />
+          <b>Профиль</b>
+        </li>
+      </ul>
+
+    </header>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
