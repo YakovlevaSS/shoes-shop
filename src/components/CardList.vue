68bd09ce0754bd2ed3124974ad1d0a3754a53e69
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-05-04 11:55:22 +0300

    click

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,10 +1,20 @@
 <script setup>
 import CardItem from './CardItem.vue'
+const onClickAdd = () => {
+    alert('1')
+}
 </script>
 
 <template>
     <div class="grid grid-cols-4 gap-5">
-        <CardItem  title="кросовки" imgUrl="/sneakers/sneakers-1.jpg" :prise="5000"/>
+        <CardItem 
+        title="кросовки" 
+        imgUrl="/sneakers/sneakers-1.jpg" 
+        :prise="5000" 
+        :isAdded="true"
+        :isFavorite="true"
+        :onClickAdd="onClickAdd"
+        />
         <CardItem />
         <CardItem />
         <CardItem />
