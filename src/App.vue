9311b141715b91a66e0d887a614483b4cb91fcc8
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2024-05-05 21:07:41 +0300

    fetch card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted } from 'vue';
+import { ref } from 'vue';
 import axios from 'axios'
 
 import HeaderComponent from './components/HeaderComponent.vue'
@@ -10,7 +11,8 @@ const items = ref([])
 
 onMounted(async () => {
   try {
-    const { data } = axios.get('');
+    const { data } = await axios.get('https://84071b17c3fd0acd.mokky.dev/items');
+    console.log(data)
     items.value = data;
   } catch (e) {
     console.log(e)
@@ -18,116 +20,6 @@ onMounted(async () => {
 
 })
 
-const items = [
-  {
-    "id": 1,
-    "title": "Мужские Кроссовки Nike Blazer Mid Suede",
-    "price": 12999,
-    "imageUrl": "/sneakers/sneakers-1.jpg",
-    "isFavorite": true,
-    "favoriteId": 3,
-    "isAdded": false
-  },
-  {
-    "id": 2,
-    "title": "Мужские Кроссовки Nike Air Max 270",
-    "price": 15600,
-    "imageUrl": "/sneakers/sneakers-2.jpg",
-    "isFavorite": true,
-    "favoriteId": 1,
-    "isAdded": false
-  },
-  {
-    "id": 3,
-    "title": "Мужские Кроссовки Nike Blazer Mid Suede",
-    "price": 8499,
-    "imageUrl": "/sneakers/sneakers-3.jpg",
-    "isFavorite": true,
-    "favoriteId": 2,
-    "isAdded": false
-  },
-  {
-    "id": 4,
-    "title": "Кроссовки Puma X Aka Boku Future Rider",
-    "price": 7800,
-    "imageUrl": "/sneakers/sneakers-4.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 5,
-    "title": "Кроссовки Future Rider",
-    "price": 9550,
-    "imageUrl": "/sneakers/sneakers-5.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 6,
-    "title": "Кроссовки Black Edition",
-    "price": 16999,
-    "imageUrl": "/sneakers/sneakers-6.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 7,
-    "title": "Кроссовки Orange Boomb Edition",
-    "price": 7499,
-    "imageUrl": "/sneakers/sneakers-7.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 8,
-    "title": "Кроссовки Nike Air Max 270",
-    "price": 15600,
-    "imageUrl": "/sneakers/sneakers-8.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 9,
-    "title": "Кроссовки Nike Air Force 1",
-    "price": 5900,
-    "imageUrl": "/sneakers/sneakers-9.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 10,
-    "title": "Кроссовки Adidas Ultraboost",
-    "price": 11500,
-    "imageUrl": "/sneakers/sneakers-10.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 11,
-    "title": "Кроссовки Puma Clyde All-Pro",
-    "price": 7600,
-    "imageUrl": "/sneakers/sneakers-11.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  },
-  {
-    "id": 12,
-    "title": "Кроссовки Converse Chuck Taylor All-Star",
-    "price": 13000,
-    "imageUrl": "/sneakers/sneakers-12.jpg",
-    "isFavorite": false,
-    "favoriteId": null,
-    "isAdded": false
-  }
-]
 </script>
 
 <template>
